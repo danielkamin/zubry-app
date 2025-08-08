833f24dcbfeb67edc909f37f5e3a4152a56437cf
Daniel Kaminski <daniel.kaminski1998@gmail.com>
2025-08-08 12:02:46 +0200

    Update map src

--- apps/frontend/src/utils/constants.ts
+++ apps/frontend/src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const CLUB_HQ_GOOGLE_MAPS_IFRAME_SRC =
   'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d439.88812299211554!2d23.095871725681672!3d53.12129886710157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ffb9c9ecc7d25%3A0xc7f595cfc728042c!2sMagnoliowa%204%2C%2015-669%20Bia%C5%82ystok!5e0!3m2!1spl!2spl!4v1627844117700!5m2!1spl!2spl';
 export const GYM_GOOGLE_MAPS_IFRAME_SRC =
-  'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d554.3122088558697!2d23.148599266056127!3d53.114481989820206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ffbfcf33a36c1%3A0xf821fdcec213ffb8!2sHala%20Sportowa%20SWIERKOWA%2020A!5e0!3m2!1spl!2spl!4v1627843437510!5m2!1spl!2spl';
+  'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2395.0592960523873!2d23.116309176827798!3d53.10912769244623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ffbea2d277c55%3A0x1c7439a498dba360!2sSzko%C5%82a%20Podstawowa%20nr%2050%20im.%20%C5%9Bw.%20Jadwigi%20Kr%C3%B3lowej%20Polski!5e0!3m2!1spl!2spl!4v1754647166028!5m2!1spl!2spl';
 export const pzKoszMainUrl = 'https://rozgrywki.pzkosz.pl/';
 export const PZKOSZ_URL = 'https://esor.pzkosz.pl/api';
 export const CMS_URL = process.env.CMS_URL;
